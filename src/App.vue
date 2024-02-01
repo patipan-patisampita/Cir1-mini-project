cfa3ec3609edd685e24ab60d25ba1ccc98eb7d41
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-01 15:37:26 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NavBar from './views/layouts/NavBar.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <nav-bar />
+  <router-view />
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+.navbar{
+  font-family: 'Sarabun', sans-serif;
+}
+
+</style>
